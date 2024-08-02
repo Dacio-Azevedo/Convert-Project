bd4180141949a41f3e9f5d8be7697186532abefc
Dacio Azevedo <dacioherazan2@gmail.com>
2024-08-02 09:18:54 -0300

    Verificação de input e conversão de moeda

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Teste");
+//Cotação de moedas do dia
+const USD = 5.75;
+const EUR = 6.23;
+const GBP = 7.33;
+
+const form = document.querySelector("form");
+const amount = document.querySelector("#amount");
+const currency = document.querySelector("#currency");
+
+// Manipulando o input amount para receber somente numeros.
+amount.addEventListener("input",() => {
+    
+    const hasCharactersRegex = /\D+/g;
+    amount.value = amount.value.replace(hasCharactersRegex, "");
+    
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    switch(currency.value){
+      case "USD":
+        convertCurrency(amount.value, USD, "$");
+        break;
+      case "EUR":
+        convertCurrency(amount.value, EUR, "€");
+        break;
+      case "GBP":
+        convertCurrency(amount.value, GBP, "£");
+        break;
+    }
+
+});
+
+//Função para converter a moeda
+function convertCurrency(amount, price, symbol){
+   console.log(amount,price,symbol);
+};
