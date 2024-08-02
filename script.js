e4f57f8d67f98a910a37c3bb9c387d04693d629f
Dacio Azevedo <dacioherazan2@gmail.com>
2024-08-02 10:07:58 -0300

    Função para mostrar o valor em Real

--- script.js
+++ script.js
@@ -6,33 +6,53 @@ const GBP = 7.33;
 const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
 const currency = document.querySelector("#currency");
+const footer = document.querySelector("main footer");
+const description = document.querySelector("#description");
 
 // Manipulando o input amount para receber somente numeros.
-amount.addEventListener("input",() => {
-    
+amount.addEventListener("input", () => {
+
     const hasCharactersRegex = /\D+/g;
     amount.value = amount.value.replace(hasCharactersRegex, "");
-    
+
 });
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    switch(currency.value){
-      case "USD":
-        convertCurrency(amount.value, USD, "$");
-        break;
-      case "EUR":
-        convertCurrency(amount.value, EUR, "€");
-        break;
-      case "GBP":
-        convertCurrency(amount.value, GBP, "£");
-        break;
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$");
+            break;
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€");
+            break;
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£");
+            break;
     }
 
 });
 
 //Função para converter a moeda
-function convertCurrency(amount, price, symbol){
-   console.log(amount,price,symbol);
+function convertCurrency(amount, price, symbol) {
+    try {
+
+        //Exebindo a cotação da moeda selecionada.
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+        footer.classList.add("show-result");
+    } catch (error) {
+        console.log(error);
+        footer.classList.remove("show-result");
+        alert("Não foi possível converter. Tente novamente mais tarde.");
+    }
 };
+
+//Formata a moeda em Real Brasileiro
+function formatCurrencyBRL(value) {
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+}    
